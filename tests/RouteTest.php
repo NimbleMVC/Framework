585d72bf9fa2dae8e909019964bca48a2b0c86b5
Krzysztof Żyłka <krzysztofzylka@yahoo.com>
2024-06-15 20:03:17 +0200

    45-testy-phpunit

---
+++ tests/RouteTest.php
@@ -0,0 +1,52 @@
+<?php
+
+use Krzysztofzylka\PdoLite\Condition;
+use Nimblephp\framework\Config;
+use Nimblephp\framework\Request;
+use Nimblephp\framework\Route;
+use PHPUnit\Framework\TestCase;
+
+class RouteTest extends TestCase
+{
+
+    public function testRoute()
+    {
+        Config::set('DEFAULT_CONTROLLER', 'index');
+        Config::set('DEFAULT_METHOD', 'index');
+        $request = new Request();
+        $route = new Route($request);
+        $this->assertEquals(
+            'index',
+            $route->getController()
+        );
+        $this->assertEquals(
+            'index',
+            $route->getMethod()
+        );
+        $this->assertEmpty($route->getParams());
+    }
+
+    public function testRoute2()
+    {
+        $_SERVER['REQUEST_URI'] = '/controller/method/param1/param2';
+        $request = new Request();
+        $route = new Route($request);
+        $this->assertEquals(
+            'controller',
+            $route->getController()
+        );
+        $this->assertEquals(
+            'method',
+            $route->getMethod()
+        );
+        $this->assertEquals(
+            [
+                'param1',
+                'param2'
+            ],
+            $route->getParams()
+        );
+        unset($_SERVER['REQUEST_URI']);
+    }
+
+}
