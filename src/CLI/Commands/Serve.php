09c0a7c1e7e12d712b1373ecdb9bc087dfb3b60f
krzysztofzylka <krzysztofzylka@yahoo.com>
2025-03-02 12:37:17 +0100

    CLI serve

--- src/CLI/Commands/Serve.php
+++ src/CLI/Commands/Serve.php
@@ -4,9 +4,18 @@ namespace NimblePHP\framework\CLI\Commands;
 
 use NimblePHP\framework\Kernel;
 
+/**
+ * Serve development app
+ */
 class Serve
 {
 
+    /**
+     * Serve
+     * @param string $host
+     * @param int $port
+     * @return void
+     */
     public function handle(string $host = '127.0.0.1', int $port = 8080): void
     {
         echo "Run server on http://$host:$port\n";
