06689cd9e90456ea8d2b8d4e9b657abca9465ae5
krzysztofzylka <krzysztofzylka@yahoo.com>
2025-03-04 20:26:04 +0100

    Upgrade exception

--- src/Exception/DatabaseException.php
+++ src/Exception/DatabaseException.php
@@ -10,11 +10,16 @@ use Throwable;
 class DatabaseException extends HiddenException
 {
 
-    public function __construct(string $message = "Database error", int $code = 500, ?Throwable $previous = null)
+    public function __construct(string $message = 'Database error', int $code = 500, ?Throwable $previous = null)
     {
+        $showMessage = 'Database error';
         $this->hiddenMessage = $message;
 
-        parent::__construct('Database error', $code, $previous);
+        if (isset($_ENV['DEBUG']) && $_ENV['DEBUG']) {
+            $showMessage = $message;
+        }
+
+        parent::__construct($showMessage, $code, $previous);
     }
 
 }
--- src/Exception/HiddenException.php
+++ src/Exception/HiddenException.php
@@ -12,11 +12,16 @@ class HiddenException extends NimbleException
 
     protected string $hiddenMessage;
 
-    public function __construct(string $message = "System error", int $code = 500, ?Throwable $previous = null)
+    public function __construct(string $message = 'System error', int $code = 500, ?Throwable $previous = null)
     {
+        $showMessage = 'System error';
         $this->hiddenMessage = $message;
 
-        parent::__construct('System error', $code, $previous);
+        if (isset($_ENV['DEBUG']) && $_ENV['DEBUG']) {
+            $showMessage = $message;
+        }
+
+        parent::__construct($showMessage, $code, $previous);
     }
 
     public function getHiddenMessage(): string
